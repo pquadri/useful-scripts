74910767239e724c9e7582656db55fa205c836da
Paolo Quadri <paolo.quadri@orobix.com>
2020-01-20 11:11:10 +0100

    new comand

--- fix_gitignore.sh
+++ fix_gitignore.sh
@@ -2,3 +2,14 @@
 git rm -r --cached .
 git add .
 git commit -m ".gitignore fix"
+
+
+# unzip and copy content to subfolder with archive name
+for i in `ls`; 
+    do unzip $i
+    mkdir ${i%.*}
+    mv *.bmp ${i%.*}/; 
+done
+
+# connect from ssh through tunnel, needs netcat configuration in .ssh/config for worker01 and bastion
+sshfs worker01:/data ~/Desktop/test -ovolname=NAME
